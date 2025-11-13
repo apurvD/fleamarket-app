7327fb5d5eb8f8198dbebe8e8197a03f7ada6f0b
Zane M <zanem@cox.net>
2025-11-13 13:08:45 -0500

    Add variety of get endpoints

--- backend/index.js
+++ backend/index.js
@@ -55,6 +55,127 @@ app.get('/vendor/:vid', (req, res) => {
   });
 });
 
+app.get('/vendor/:vid/product', (req, res) => {
+  db.query(`SELECT * FROM product where vid = ${req.params["vid"]}`, (err, results) => {
+    if (err) {
+      console.error('Error executing query: ' + err.stack);
+      res.status(500).send('Error fetching products');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.get('/vendor/:vid/sale', (req, res) => {
+  db.query(`SELECT DISTINCT s.* FROM sale s JOIN saleitem si ON si.sid = s.id JOIN product ON si.pid = p.id where p.vid = ${req.params["vid"]}`, (err, results) => {
+    if (err) {
+      console.error('Error executing query: ' + err.stack);
+      res.status(500).send('Error fetching sales');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.get('/sale/:sid', (req, res) => {
+  db.query(`SELECT * FROM sale s where id = ${req.params["sid"]}`, (err, results) => {
+    if (err) {
+      console.error('Error executing query: ' + err.stack);
+      res.status(500).send('Error fetching sales');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.get('/sale/:sid/item', (req, res) => {
+  db.query(`SELECT * FROM saleitem where sid = ${req.params["sid"]}`, (err, results) => {
+    if (err) {
+      console.error('Error executing query: ' + err.stack);
+      res.status(500).send('Error fetching saleitems');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.get('/sale/:sid/product', (req, res) => {
+  db.query(`SELECT * FROM saleitem si JOIN product p ON si.pid = p.id where si.sid = ${req.params["sid"]}`, (err, results) => {
+    if (err) {
+      console.error('Error executing query: ' + err.stack);
+      res.status(500).send('Error fetching saleitems');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.get('/sale/:sid/vendor', (req, res) => {
+  db.query(`SELECT DISTINCT v.* FROM saleitem si JOIN product p ON si.pid = p.id JOIN vendor v ON p.vid = v.id where si.sid = ${req.params["sid"]}`, (err, results) => {
+    if (err) {
+      console.error('Error executing query: ' + err.stack);
+      res.status(500).send('Error fetching vendors');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.get('/product/:pid', (req, res) => {
+  db.query(`SELECT * FROM product where id = ${req.params["pid"]}`, (err, results) => {
+    if (err) {
+      console.error('Error executing query: ' + err.stack);
+      res.status(500).send('Error fetching products');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.get('/product/:pid/item', (req, res) => {
+  db.query(`SELECT * FROM saleitem where pid = ${req.params["pid"]}`, (err, results) => {
+    if (err) {
+      console.error('Error executing query: ' + err.stack);
+      res.status(500).send('Error fetching saleitems');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+routes.get('/booth', (req, res) => {
+  db.query(`SELECT * FROM booth`, (err, results) => {
+    if (err) {
+      console.error('Error executing query: ' + err.stack);
+      res.status(500).send('Error fetching booths');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.get('/booth/:bid', (req, res) => {
+  db.query(`SELECT * FROM booth where id = ${req.params["bid"]}`, (err, results) => {
+    if (err) {
+      console.error('Error executing query: ' + err.stack);
+      res.status(500).send('Error fetching booths');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.get('/booth/:bid/reservation', (req, res) => {
+  db.query(`SELECT * FROM reservation where bid = ${req.params["bid"]}`, (err, results) => {
+    if (err) {
+      console.error('Error executing query: ' + err.stack);
+      res.status(500).send('Error fetching reservations');
+      return;
+    }
+    res.json(results);
+  });
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
